2d17bfaf9bfd93f3aaca5f3a875b4a1af6c269e1
Mark Matney <mmatney@library.ucla.edu>
2016-11-03 14:35:19 -0700

    implement EMEL-301 scripts

--- EMEL/move-to-default-includes.sh
+++ EMEL/move-to-default-includes.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
 # Move all image files and associated XMP files in each folio that contain
-# certain substrings in their filenames to a new subdirectory called
-# 'default includes', and compile a list of all files that were moved for each
-# manuscript.
+# certain substrings in their filenames to a default includes directory
+# (inside each folio directory), and compile a list of all default included
+# files for each manuscript.
+#
+#
+# Usage:
+#
+#   ./move-to-default-includes.sh substring-list /dir/containing/manuscripts
+#
+#   1. substring-list is a text file (named anything) that contains a list of
+#      substrings; files that contain them will be moved to a default_includes
+#      folder (one folder for each folio) and added to the listfile of included
+#      files (one such file per manuscript)
+#
+#   2. The path to the directory that contains manuscripts should not end with
+#      a slash
+#
 #
 # In other words, provided a file that contains a newline-separated list of substrings:
 #
 #   aaa
 #   ccc
 #
-# the filesystem like goes from:
+# the filesystem goes from:
 #
 #   manuscript1/
 #     folio1/
@@ -55,3 +69,61 @@
 #   image1ccc
 #   image2aaa
 #   image2ccc
+
+if [ ! -e $1 ]
+then
+    echo $1 'does not exist!'
+    exit 1
+fi
+
+SUBSTRINGS=$(tr -d '\r' < $1)
+MANUSCRIPTS_DIR=$2
+MANUSCRIPT_DIRS=$(find ${MANUSCRIPTS_DIR} -mindepth 1 -maxdepth 1 -type d)
+
+for manuscript_dir in ${MANUSCRIPT_DIRS}
+do
+    DEFAULT_INCLUDED_FILE=${manuscript_dir}/default_included.txt 
+    TMP_DEFAULT_INCLUDED_FILE=${manuscript_dir}/tmp_default_included
+
+    # make sure default_included.txt exists
+    touch ${DEFAULT_INCLUDED_FILE}
+    touch ${TMP_DEFAULT_INCLUDED_FILE}
+
+    FOLIO_DIRS=$(find ${manuscript_dir} -mindepth 1 -maxdepth 1 -type d)
+    for folio_dir in ${FOLIO_DIRS}
+    do
+        DEFAULT_INCLUDES_DIR=${folio_dir}/default_includes
+        mkdir ${DEFAULT_INCLUDES_DIR}
+
+        for substring in ${SUBSTRINGS}
+        do
+            # move all files in the curent subdirectory that contain 'substring'
+            # in the filename to 'default_includes' (both images and metadata)
+
+            MATCHING_FILES=$(find ${folio_dir} -mindepth 1 -maxdepth 1 -type f \
+                -name '*'${substring}'*')
+
+            if [[ ${MATCHING_FILES} != '' ]]
+            then
+                mv --target-directory=${DEFAULT_INCLUDES_DIR} ${MATCHING_FILES}
+
+                # write image file names to tmp
+
+                echo ${MATCHING_FILES} \
+                    | sed -e 's/ \+/\n/g' \
+                    | sed -e '/^.*\.xmp$/d' \
+                    >> ${TMP_DEFAULT_INCLUDED_FILE}
+            fi
+        done
+    done
+
+    # add new filenames to DEFAULT_INCLUDED_FILE
+
+    cat ${DEFAULT_INCLUDED_FILE} ${TMP_DEFAULT_INCLUDED_FILE} \
+        | sort -u \
+        | tee ${DEFAULT_INCLUDED_FILE}
+
+    # remove tmp file
+
+    rm ${TMP_DEFAULT_INCLUDED_FILE}
+done
---
+++ EMEL/undo-move-to-default-includes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script undoes 'move-to-default-includes.sh':
+#   1. Deletes the default_included file
+#   2. Moves all files out of the default_includes directories and back into
+#      their folio directory
+#   3. Deletes all (empty) default_includes directories
+#
+# Usage:
+#
+#   ./undo-move-to-default-includes.sh /dir/containing/manuscripts
+#
+#   1. The path of the directory containing manuscripts must not end in a slash
+
+MANUSCRIPTS_DIR=$1
+MANUSCRIPT_DIRS=$(find ${MANUSCRIPTS_DIR} -mindepth 1 -maxdepth 1 -type d)
+
+for manuscript_dir in ${MANUSCRIPT_DIRS}
+do
+    DEFAULT_INCLUDED_FILE=${manuscript_dir}/default_included.txt 
+    rm ${DEFAULT_INCLUDED_FILE}
+
+    FOLIO_DIRS=$(find ${manuscript_dir} -mindepth 1 -maxdepth 1 -type d)
+    for folio_dir in ${FOLIO_DIRS}
+    do
+        DEFAULT_INCLUDES_DIR=${folio_dir}/default_includes
+        mv ${DEFAULT_INCLUDES_DIR}/* ${folio_dir}
+        rmdir ${DEFAULT_INCLUDES_DIR}
+    done
+done
