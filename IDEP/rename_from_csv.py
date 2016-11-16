d33e3028742f5abc34f2537490baa7323d80542f
Dawn Childress <kirschbombe@gmail.com>
2016-11-15 18:46:20 -0800

    add rename_from_cvs.py

--- IDEP/rename_from_csv.py
+++ IDEP/rename_from_csv.py
@@ -1,8 +1,26 @@
 #!/usr/local/bin/python
 
+
+# Usage
+#
+#  $ python rename_from_csv.py
+#
+#  1. Assumes you have a file "rename.csv" with two columns. Column one is old filename, column two is new filename.
+#  2. "rename.csv" should be in the parent folder
+#  3. This file ("rename_from_csv.py") should be in the folder with the files to be renamed
+#
+#  Example directory structure:
+#
+#  project/
+#      rename.csv
+#      filestorename/
+#          file1.tif
+#          file2.tif
+#          rename_from_csv.py
+
 import os, csv, sys
 
-with open('../khiz.csv','rb') as csvfile:
+with open('../rename.csv','rb') as csvfile:
     reader = csv.reader(csvfile, delimiter = ',')
     for row in reader:
        oldname = row[0]
