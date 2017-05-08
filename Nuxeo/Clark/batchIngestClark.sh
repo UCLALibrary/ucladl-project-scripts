82a2869de844e80246c588a713411349ea2eeeb6
Dawn Childress <kirschbombe@users.noreply.github.com>
2017-05-08 13:46:41 -0700

    Create batchIngestClark.sh

---
+++ Nuxeo/Clark/batchIngestClark.sh
@@ -0,0 +1,12 @@
+# Batch ingest images from a directory that shares the same name as the folder or folderish object in Nuxeo.
+# "${PWD##*/}" prints the current directory w/out path.
+# Used for Clark manuscripts.
+# Imports all .tif files in the directory.
+# Should be run from the same directory as the files. 
+# For similar collections, replace "clark/mss" with appropriate path.
+
+xargs -I {} \
+    nx upfile \
+      -dir \
+      /asset-library/UCLA/clark/mss/"${PWD##*/}" \
+       {} < ls *.tif
