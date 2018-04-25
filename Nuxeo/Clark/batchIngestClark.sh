b4ef5892cbdb8747414ac708457daeb25e55e754
U-AD\dchildress <dchildress@YRL-WV-DLP601.ad.library.ucla.edu>
2018-04-25 08:57:20 -0700

    update clark ingest

--- Nuxeo/Clark/batchIngestClark.sh
+++ Nuxeo/Clark/batchIngestClark.sh
@@ -5,10 +5,19 @@
 # Should be run from the same directory as the files.
 # For similar collections, replace "clark/mss" with appropriate path.
 
-set -euo pipefail
+# Batch ingest images from a directory that shares the same name as the folder or folderish object in Nuxeo.
+# "${PWD##*/}" prints the current directory w/out path.
+# Used for Clark manuscripts.
+# Imports all .tif files in the directory.
+# Should be run from the same directory as the files.
+# For similar collections, replace "clark/mss" with appropriate path.
+
+#! /bin/bash
+set -e
+set -o pipefail
 
 xargs -I {} \
     nx upfile \
       -dir \
-      /asset-library/UCLA/clark/annotated/"${PWD##*/}" \
+      /asset-library/UCLA/clark/mss/"${PWD##*/}" \
        {} < ls *.tif
