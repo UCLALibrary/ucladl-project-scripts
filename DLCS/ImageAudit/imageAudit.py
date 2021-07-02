e6b0c49291794c40b074a9bca41401ed3b229733
ztucker4 <tuckerzoej@gmail.com>
2021-07-02 15:50:01 -0700

    Add refactored versions of two scripts to increase testability

--- DLCS/ImageAudit/imageAudit.py
+++ DLCS/ImageAudit/imageAudit.py
@@ -2,11 +2,55 @@ import argparse
 import csv
 import os
 import pandas as pd
-print()
+from pathlib import Path
+from pathlib import PureWindowsPath, PurePosixPath
+
 def list_diff(l_1,l_2):
     '''returns list of items in first list, but not second'''
     return(list(set(l_1) - set(l_2)))
 
+def get_merged_lists(csv_list, disk_list):
+    csv_extras = list_diff(file_list_csv, file_list_disk)
+    disk_extras = list_diff(file_list_disk, file_list_csv)
+    out_dict_list = []
+    for file in csv_list:
+        if file in csv_extras:
+            on_disk = False
+            on_csv = True
+        else:
+            on_disk = True
+            on_csv = True
+        #format item name as it will be in spreadsheet
+        file = os.path.relpath(file, args.filepath)
+        file = str(PurePosixPath(PureWindowsPath(file)))
+        out_dict_list.append({'File Name': file,
+                              'On disk': on_disk,
+                              'On CSV': on_csv})
+    for file in disk_extras:
+        on_disk = True
+        on_csv = False
+        #format item name as it will be in spreadsheet
+        file = os.path.relpath(file, args.filepath)
+        file = str(PurePosixPath(PureWindowsPath(file)))
+        out_dict_list.append({'File Name': file,
+                              'On disk': on_disk,
+                              'On CSV': on_csv})
+    return(out_dict_list)
+    
+
+def write_final_csv(csv, dict_list):
+    csv_df = pd.read_csv(csv)
+    #add columns for presence on disk and CSV, default to true
+    csv_df['On disk'] = 'True'
+    csv_df['On CSV'] = 'True'
+    
+    for item in dict_list:
+        if item['On CSV'] == True and item['On disk'] == False:
+            csv_df.loc[csv_df['File Name'] == item['File Name'], 'On disk'] = False
+        elif item['On CSV'] == False and item['On disk'] == True:
+            csv_df = csv_df.append(item, ignore_index = True)
+    print(csv_df)
+    csv_df.to_csv(args.input_file, index=False)
 
 #parse command line arguments
 parser = argparse.ArgumentParser(description='Run audit of file inventory csv \
@@ -14,49 +58,27 @@ parser = argparse.ArgumentParser(description='Run audit of file inventory csv \
 parser.add_argument('input_file',metavar='N', type=str,
                     help='filename of input csv to audit against')
 parser.add_argument('filepath',metavar='N', type=str, nargs='?',
-                    default = r'C:\Users\tucke\TestImages',
+                    default = Path(r'C:\Users\tucke\TestImages'),
                     help='path to files to audit, default = DEFAULT')
 args = parser.parse_args()
 
 #read CSV file
-csv_df = pd.read_csv(args.input_file)
+csv_df_initial = pd.read_csv(args.input_file)
 file_list_csv = []
-raw_list_csv = csv_df['File Name'].tolist()
+raw_list_csv = csv_df_initial['File Name'].tolist()
 
 for row in raw_list_csv:
     if not pd.isna(row):
-        filename = row.replace('/', '\\')
-        file_list_csv.append(args.filepath + '\\' + filename)
+        filename = Path(row)
+        file_list_csv.append(os.path.join(args.filepath, filename))
 
 #read directory contents
 file_list_disk = []
 for root, dirs, files in os.walk(args.filepath):
     for name in files:
-        fullPath = root + '\\'+  name
+        fullPath = os.path.join(root, name)
         file_list_disk.append(fullPath)
 
-#add columns for presence on disk and CSV, default to true
-csv_df['On disk'] = 'True'
-csv_df['On CSV'] = 'True'
-
-#Files on CSV, but not on disk
-csv_extras = list_diff(file_list_csv, file_list_disk)
-for item in csv_extras:
-    item_name = item.replace(args.filepath, '')
-    item_name_format = item_name.replace('\\', '/')[1:]
-    csv_df.loc[csv_df['File Name'] == item_name_format, 'On disk'] = 'False'
-
-#files on disk, but not csv
-disk_extras = list_diff(file_list_disk, file_list_csv)
-
-for item in disk_extras:
-    item_name = item.replace(args.filepath, '')
-    item_name_format = item_name.replace('\\', '/')[1:]
-    row_dict = {'File Name': item_name_format,
-                'On disk': 'True',
-                'On CSV': 'False'}
-    
-    csv_df = csv_df.append(row_dict, ignore_index = True)
-
-#write back to same CSV
-csv_df.to_csv(args.input_file, index=False)
+row_dicts = get_merged_lists(file_list_csv, file_list_disk)
+print(row_dicts)
+write_final_csv(args.input_file, row_dicts)
