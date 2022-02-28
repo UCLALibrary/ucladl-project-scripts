a0533d72bb9f2260a2fc2f73742bfd7d8d9bde77
ztucker4 <tuckerzoej@gmail.com>
2022-02-28 14:44:12 -0800

    revise metadata script with bug fixes

--- MEAP/MEAP_migrator.py
+++ MEAP/MEAP_migrator.py
@@ -5,7 +5,7 @@ import os
 from pathlib import Path
 from pathlib import PureWindowsPath, PurePosixPath
 
-def map_concat_cols(input_df, output_df):
+def map_concat_cols(input_df, output_df, works_path):
 
     candidate_cols = ['Alt Title','English Translation of Title','Alternative Title']
     present_cols = []
@@ -13,6 +13,7 @@ def map_concat_cols(input_df, output_df):
         if col in input_df.columns:
             present_cols.append(col)
     output_df['AltTitle.other'] = input_df[present_cols].apply(lambda row: '|~|'.join(row.values.astype(str)), axis=1)
+    output_df['AltTitle.other'] = output_df['AltTitle.other'].replace({'nan':''})
 
     candidate_cols = ['Abstract','Abstract | alb','Abtract | eng','Abstract | por']
     present_cols = []
@@ -20,15 +21,17 @@ def map_concat_cols(input_df, output_df):
         if col in input_df.columns:
             present_cols.append(col)
     output_df['Summary'] = input_df[present_cols].apply(lambda row: '|~|'.join(row.values.astype(str)), axis=1)
-    
+    output_df['Summary'] = output_df['Summary'].replace({'nan':''})
+	
     candidate_cols =['local ID', 'collection number']
     present_cols = []
     for col in candidate_cols:
         if col in input_df.columns:
             present_cols.append(col)
     output_df['AltIdentifier.local'] = input_df[present_cols].apply(lambda row: ' | '.join(row.values.astype(str)), axis=1)
+    output_df['AltIdentifier.local'] = output_df['AltIdentifier.local'].replace({'nan':''})
 
-    output_df['File Name'] = 'Masters/othermasters/MEAPmigration/ramakatanearchive' + input_df['File name']
+    output_df['File Name'] = works_path + input_df['File name']
 
     
     return output_df
@@ -101,18 +104,18 @@ def add_item_pages(items_directory,works_df):
     items_df = pd.DataFrame(columns=destination_cols)
     item_files = sorted(os.listdir(items_directory))
     filenames = works_df['File Name'][1:]
-    print('Starting adding Page items. Total Work files: ' + str(len(filenames)))
+    print('Starting adding Page items. Total Work entries: ' + str(len(filenames)))
     tenpct = round(len(filenames)/10)
     i=0
     pct = 0
     pct_count = 0
     for filename in filenames:
         name = os.path.split(filename)[1]
-        file_num = (name.split('.')[0]).split('_')[-1]
+        file_ID = (name.split('.')[0])
         child_items = []
 
         for item in item_files:
-            if file_num in item:
+            if file_ID in item:
                 child_items.append(item)
         parent_ark = works_df.loc[works_df['File Name'] == filename,'Item ARK'].iloc[0]
 
@@ -132,7 +135,16 @@ def add_item_pages(items_directory,works_df):
             pct_count = pct_count+10
     return items_df
 
-def main(input_directory, items_directory, viewing_hint):
+def main():
+    input_directory = input("Enter the directory containing metadata files: ")
+    works_directory = input("Enter the directory containing WORK file (e.g. Masters\othermasters...): ")
+    complex = input("Is this a complex (multiple pages per work) collection? Y/N: ")
+	
+    items_directory = ''
+    viewing_hint = ''
+    if complex == "Y" or complex == "y":
+        items_directory = input("Enter the directory containing child item files: ")
+        viewing_hint = input("Enter the viewing hint for all child items: ")
 
     for name in os.listdir(input_directory):
         if 'collection' in name:
@@ -166,7 +178,7 @@ def main(input_directory, items_directory, viewing_hint):
     output_df = pd.DataFrame(columns=destination_cols)
     
     print('Mapping work-level metadata')
-    output_df = map_concat_cols(full_input_df, output_df)
+    output_df = map_concat_cols(full_input_df, output_df, works_directory)
     output_df = map_constant_cols(output_df,items_directory,viewing_hint)
     output_df = map_simple_cols(full_input_df,output_df)
     
@@ -183,13 +195,5 @@ def main(input_directory, items_directory, viewing_hint):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input_directory',type=str,
-                        help=r'directory of input metadata csvs')
-    parser.add_argument('items_directory',type=str,
-                        help = 'paginated material only - directory of child items')
-    parser.add_argument('viewing_hint',type=str,
-                        help = 'string to use as viewing hint for child items (complex only)')
-    args = parser.parse_args()
-    main(args.input_directory, args.items_directory, args.viewing_hint)
+    main()
 
